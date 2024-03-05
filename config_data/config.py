2f126ae1aca3f07f66c6377cfbbff4a688f015f4
Айдын Карамулдинов <66464203+aidynsemey7sk@users.noreply.github.com>
2024-03-05 18:18:06 +0600

    Update config.py

--- config_data/config.py
+++ config_data/config.py
@@ -16,4 +16,4 @@ class Config:
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'), admin_ids=eval(env('ADMINS'))))
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
